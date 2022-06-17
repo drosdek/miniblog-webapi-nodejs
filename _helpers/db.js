320eb24199173c5a5f4d3769e86d55fffee3d3c6
Josnei Drosdek <josneidrosdek@gmail.com>
2022-06-17 19:44:24 -0300

    Implementado configurações para tests;

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,10 +1,20 @@
-const config = require('config.json');
-const mongoose = require('mongoose');
-const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+const config = require("config");
+const mongoose = require("mongoose");
+const connectionOptions = {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
 
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    `mongodb+srv://${config.username}:${config.password}@${config.cluster}.mongodb.net/${config.dbname}?retryWrites=true&w=majority`,
+  connectionOptions
+);
+
 mongoose.Promise = global.Promise;
 
 module.exports = {
-    Author: require('../models/authors.model')
+  Author: require("../models/authors.model"),
 };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var config = require("config");
 
 var indexRouter = require("./routes/index");
 var authorsRouter = require("./routes/authors");
@@ -10,18 +11,15 @@ const errorHandler = require("./_helpers/error_handle");
 
 var app = express();
 
-app.use(logger("dev"));
+if (config.util.getEnv("NODE_ENV") !== "test") {
+  app.use(logger("combined")); //'dev' outputs the Apache style LOGs
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/", indexRouter);
 app.use("/authors", authorsRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
 
 // error handler
 app.use(errorHandler);
